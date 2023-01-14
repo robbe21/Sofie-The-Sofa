b02ab2b41a8296d122a2a71772137c1d8814c90a
Robbe Van Kets <robbe.van.kets@rws.nl>
2023-01-14 19:31:32 +0000

    Add chair talks first

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import serial
 import time
 
 r = sr.Recognizer()
-
 mic = sr.Microphone()
 test = sr.AudioFile('test.flac')
 print("Start talking")
@@ -20,34 +19,40 @@ print(result)
 
 ##    ## 
 
-# set up the serial line
-ser = serial.Serial('/dev/cu.usbmodem2101', 9600)
-time.sleep(2)
+## Hey isabel hier kan je dingentjes aanpassen#
 
+lichtsensorthreshold = 200
+
+
+###
+# set up the serial line
+lichtsensor = 1000
 
 # Read and record the data
-data =[]                       # empty list to store the data
-for i in range(5):
-    b = ser.readline()         # read a byte string
-    string_n = b.decode()  # decode byte string into Unicode  
-    string = string_n.rstrip() # remove \n and \r
-    flt = float(string)        # convert string to float
-    print(flt)
-    data.append(flt)           # add to the end of data list
-    # time.sleep(0.1)            # wait (sleep) 0.1 seconds
+while lichtsensor > lichtsensorthreshold:
+    ser = serial.Serial('/dev/cu.usbmodem2101', 9600)
+    data =[]                       # empty list to store the data
+    for i in range(1):
+        b = ser.readline()         # read a byte string
+        string_n = b.decode()  # decode byte string into Unicode  
+        string = string_n.rstrip() # remove \n and \r
+        flt = float(string)        # convert string to float
+        print(flt)
+        data.append(flt)           # add to the end of data list
+        lichtsensor = data[0]
 
-ser.close()
+    ser.close()
 ##AI
 language = 'en'
-if data[0] > 200:
-    myobj = gTTS(text="Please sit on me to talk", lang=language, slow=False)
-    myobj.save("welcome.mp3")
-    os.system("mpg321 welcome.mp3")
-    print("KOM OP MIJ ZITTEN")
-    exit()
+# if data[0] > lichtsensorthreshold:
+myobj = gTTS(text="Hey how are you? Got any questions for me?", lang=language, slow=False)
+myobj.save("welcome.mp3")
+os.system("mpg321 welcome.mp3")
+# print("KOM OP MIJ ZITTEN")
+# exit()
 openai.api_key = "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"
 
-response = openai.Completion.create(model="text-davinci-003", prompt="You are a chair please answer this: " + "?"+ result, temperature=0, max_tokens=4097 - (result.__len__()+20))
+response = openai.Completion.create(model="text-davinci-003", prompt="You are a chair you are made from wool and your creators name is Isabel Brems. please answer this: " + result + "?", temperature=0, max_tokens=4097 - (result.__len__()+20))
 
 answer = response["choices"][0]["text"]
 print(answer)
